c8c6740a024d4935f6e355e9971a960a97391742
Pravin Pande <pravin.pande145@gmail.com>
2020-02-28 11:03:31 -0500

    Update SimpleSearch.py

--- SimpleSearch.py
+++ SimpleSearch.py
@@ -1,3 +1,7 @@
+# Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest) 
+# and another number. The function decides whether or not the given number is inside the list and returns (then prints) 
+# an appropriate boolean.
+
 def simplesearch(alist,anum):
     for i in alist:
         if i == anum:
